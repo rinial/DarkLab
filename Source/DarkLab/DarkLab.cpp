cbc998d8a915a5624c85fe8b641c980564af57cf
Niseite <FJMK2011@gmail.com>
2018-04-08 03:30:44 +0300

    Initial commit

---
+++ Source/DarkLab/DarkLab.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "DarkLab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DarkLab, "DarkLab" );
+
+DEFINE_LOG_CATEGORY(LogDarkLab)
+ 
