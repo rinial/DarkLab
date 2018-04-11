b33f6b95dee30b89ceb82a47bb2842c26edf7c97
Niseite <FJMK2011@gmail.com>
2018-04-11 19:50:23 +0300

    Delete Geometry folder. Update scalability

--- Source/DarkLab/DarkLabCharacter.cpp
+++ Source/DarkLab/DarkLabCharacter.cpp
@@ -33,7 +33,7 @@ ADarkLabCharacter::ADarkLabCharacter()
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->bAbsoluteRotation = true; // Don't want arm to rotate when character does
-	CameraBoom->TargetArmLength = 800.f;
+	CameraBoom->TargetArmLength = 1200.f;
 	CameraBoom->RelativeRotation = FRotator(-60.f, 0.f, 0.f);
 	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
 
